752fa743eaa0dcde7a0cc79bed1f24a0b13f3fd7
andreamaggioni <maggioni.andrea93@gmail.com>
2020-01-08 09:14:52 +0000

    init commit

---
+++ src/main/java/com/maggio/telegram/togglbot/TogglbotApplication.java
@@ -0,0 +1,13 @@
+package com.maggio.telegram.togglbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogglbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogglbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maggio/telegram/togglbot/TogglbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maggio.telegram.togglbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogglbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
